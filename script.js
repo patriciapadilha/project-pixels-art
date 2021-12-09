bd9d29bbe6e4fbffe67b5e709f260b644e631f0f
Patricia Padilha <patriciapadilhabio@gmail.com>
2021-12-09 19:28:11 -0300

    arruma identação

--- script.js
+++ script.js
@@ -1,70 +1,63 @@
 window.onload = function () { 
+
     
     //Cor inicial padrão black;
     let inicialColor = document.getElementById('colorBlack');
     inicialColor.className ='color selected';
     //Selecionar cor;
-
+    
     function selectColor() {
         let paletteColors = document.querySelectorAll('.color');
         
         for (let index = 0; index < paletteColors.length; index += 1) {
             paletteColors[index].addEventListener('click', function (event) {
-               for (let index = 0; index < paletteColors.length; index += 1) {
-                  if ( event.target.className === 'color'  && event.target.className === 'selected'){
-                    event.target.className = 'color';
-                  } else {
-                    event.target.className = 'color selected';
-                  }
-               }
-            
-              console.log(event.target.classList);
-              console.log(event.target.className);
+                
+                document.querySelector('.selected').classList.remove('selected');
+                event.target.classList.add('selected');
+                
+               // console.log(event.target.classList);
+               // console.log(event.target.className);
             });
         }
     }
-    
+                  
     selectColor();
-    
-    
-  function changeColor () {
-
-      let selectPixel = document.querySelectorAll('#pixel-board>.pixel');
-       for (let index = 0; index < selectPixel.length; index += 1) { 
-          selectPixel[index].addEventListener('click', function (event) {
-              
-              let selectedColor = document.querySelector('.selected');
-              let colorBackground = window
-              .getComputedStyle(selectedColor, null)
-              .getPropertyValue('background-color');
-              
-              event.target.style.backgroundColor = colorBackground;  
-            })
-              
+                
+                
+    function changeColor () {
+                    
+        let selectPixel = document.querySelectorAll('#pixel-board>.pixel');
+        for (let index = 0; index < selectPixel.length; index += 1) { 
+            selectPixel[index].addEventListener('click', function (event) {
+                let selectedColor = document.querySelector('.selected');
+                let colorBackground = window
+                .getComputedStyle(selectedColor, null)
+                .getPropertyValue('background-color');
+                
+                event.target.style.backgroundColor = colorBackground;  
+            })              
         }
     }
-        
+                
     changeColor ()   
-        
+                
+                
     function cleanBoard () {
-
+                    
         let clearButton = document.querySelector('#clear-board>button');
         // console.log(clearButton);
         clearButton.addEventListener('click' , function (){
             let selectPixel = document.querySelectorAll('#pixel-board>.pixel');
-            for (let index = 0; index < selectPixel.length; index += 1) { 
-               selectPixel[index].addEventListener('click', function (event) {
-              
-                event.target.style.backgroundColor = 'white';  
-               })
-              
-            }
+                for (let index = 0; index < selectPixel.length; index += 1) { 
+                    selectPixel[index].addEventListener('click', function (event) {
+                        event.target.style.backgroundColor = 'white';          
+                    })
+                            
+                }
         })
-
+                    
     }
-
     cleanBoard ()
-        
-        
-    
+                
 }
+
