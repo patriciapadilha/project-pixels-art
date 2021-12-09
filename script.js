e7756402923d0026546ad848c1929682b0b5f68b
Patricia Padilha <patriciapadilhabio@gmail.com>
2021-12-09 18:49:22 -0300

    função selecionar cor e função mudar cor

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-window.onload = function () {
+window.onload = function () { 
+    
+    //Cor inicial padrão black;
+    let inicialColor = document.getElementById('colorBlack');
+    inicialColor.className ='color selected';
+    //Selecionar cor;
 
- 
-   let inicialColor = document.getElementById('colorBlack');
-   inicialColor.className ='color seleted';
+    function selectColor() {
+        let paletteColors = document.querySelectorAll('.color');
+        
+        for (let index = 0; index < paletteColors.length; index += 1) {
+            paletteColors[index].addEventListener('click', function (event) {
+               for (let index = 0; index < paletteColors.length; index += 1) {
+                  if ( event.target.className === 'color'  && event.target.className === 'selected'){
+                    event.target.className = 'color';
+                  } else {
+                    event.target.className = 'color selected';
+                  }
+               }
+            
+              console.log(event.target.classList);
+              console.log(event.target.className);
+            });
+        }
+    }
+    
+    selectColor();
+    
+    
+  function changeColor () {
 
-//    function clickColor (){
-// }
-  let selectColor = document.querySelectorAll('#color-palette>.color');
-   console.log(selectColor);
+      let selectPixel = document.querySelectorAll('#pixel-board>.pixel');
+       for (let index = 0; index < selectPixel.length; index += 1) { 
+          selectPixel[index].addEventListener('click', function (event) {
+              
+              let selectedColor = document.querySelector('.selected');
+              let colorBackground = window
+              .getComputedStyle(selectedColor, null)
+              .getPropertyValue('background-color');
+              
+              event.target.style.backgroundColor = colorBackground;  
+            })
+              
+        }
+    }
+        
+    changeColor ()   
+        
+    // function clean () {
 
-
-
-
-
+    // }
+        
+        
+    
 }
