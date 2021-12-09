8480ff26c62e0298d59d435cce94709aa9d96dcf
Patricia Padilha <patriciapadilhabio@gmail.com>
2021-12-09 16:29:47 -0300

    inicio da implementação com js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+
+ 
+   let inicialColor = document.getElementById('colorBlack');
+   inicialColor.className ='color seleted';
+
+//    function clickColor (){
+// }
+  let selectColor = document.querySelectorAll('#color-palette>.color');
+   console.log(selectColor);
+
+
+
+
+
+}
